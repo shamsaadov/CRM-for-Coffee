bd998622f96f932557121390e8f25e7b365aac5c
Said <shamsadov@MacBook-Pro-shamsadov.local>
2021-08-26 14:46:57 +0300

    Ридми файл

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,6 +17,7 @@ function App() {
       <Router>
         <Switch>
 
+
           <Route exact path='/'>
             <Header/>
             <Home/>
--- config.js
+++
@@ -1,9 +0,0 @@
-const PORT = 9000;
-
-const mongoose =
-  "mongodb+srv://shamsadov:121314qq@cluster0.8m6ff.mongodb.net/CoffeeSoul";
-
-module.exports = {
-  PORT,
-  mongoose,
-};
